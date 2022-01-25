840ce0464412c34f4567fe4ad723bae6c3e54ea9
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-01-25 23:07:56 +0000

    added firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCH9dfa0AtH2ADyhSxGwSkmyQUju7GTNpE",
+  authDomain: "auth-todolist-4056f.firebaseapp.com",
+  projectId: "auth-todolist-4056f",
+  storageBucket: "auth-todolist-4056f.appspot.com",
+  messagingSenderId: "159819673001",
+  appId: "1:159819673001:web:b18dfbcfcdee77d004e8b8",
+};
